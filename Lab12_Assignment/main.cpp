529e8f1049b94153b10e70d17d8c846389083f65
Serena Gibbons <serenalgibbons@gmail.com>
2019-06-04 09:22:47 -0400

    Initial commit

---
+++ Lab12_Assignment/main.cpp
@@ -0,0 +1,49 @@
+// Serena Gibbons
+// BCS230
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* A palindrome is a string that reads the same both forward and backward. For example, the
+string "madam" is a palindrome. Write a program that uses a recursive function to check whether 
+a string is a palindrome. Your program must contain a value-returning function that returns 
+true if the string is a palindrome and false otherwise. Do not use any global variables, use the 
+appropriate parameters. */
+
+bool isPalindrome(string, int, int);
+
+int main() {
+
+	string str;
+	cout << "Palindrome Checker\n";
+	cout << "Enter a string to check whether it is a palindrome: ";
+	cin >> str;
+
+	int pos = 0;						// position
+	int end = str.length() - 1;			// end position of string array
+
+	for (str; pos < end; pos++)			// if string contains uppercase letters, convert to lowercase
+		str[pos] = tolower(str[pos]);	// isPalindrome will not recognize uppercase and lowercase as the same char
+	
+	//isPalindrome(str, pos, end);
+
+	cout << endl;
+	if (isPalindrome(str, pos, end) == 1)
+		cout << "The string \"" << str << "\" is a palindrome.\n";
+	else
+		cout << "The string \"" << str << "\" is not a palindrome.\n";
+
+	system("pause");
+	return 0;
+}
+
+bool isPalindrome(string s, int p, int e) {
+	if (p > e)						// base case
+		return false;
+	else if (s[p] == s[e]) {		// check if the first element is the same as the last
+		isPalindrome(s, p+1, e-1);
+		return true;
+	}
+	return false;
+}
